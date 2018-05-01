00390eded5a1bf9b4f6f880e7a6d95dbd7848b42
tij4sto <j.stolzenberg@ostfalia.de>
2018-05-01 14:41:28 +0200

    Db Update

--- app/src/main/java/de/s/j/vorsorge_james/database/dbKind/DbKindAccessHelper.java
+++ app/src/main/java/de/s/j/vorsorge_james/database/dbKind/DbKindAccessHelper.java
@@ -7,15 +7,25 @@ import android.util.Log;
 
 public class DbKindAccessHelper extends SQLiteOpenHelper {
 
-    private static final String LOG_TAG = DbKindAccessHelper.class.getSimpleName();
+    public static final String LOG_TAG = DbKindAccessHelper.class.getSimpleName();
+
+    public static final String DB_NAME = "vorsorge-james.db";
+    public static final int DB_VERSION = 1;
+
+    public static final String TABLE_KIND = "Kind";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_GEBURTSTAG = "geburtstag";
 
     public static final String SQL_CREATE = "CREATE TABLE " + TABLE_KIND +
-            "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NAME + " TEXT NOT NULL, "
-            + COLUMN_GEBURTSTAG
+            "("
+            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_NAME + " TEXT NOT NULL, "
+            + COLUMN_GEBURTSTAG +  " TEXT NOT NULL);";
 
 
     public DbKindAccessHelper(Context context) {
-        super(context, "PLATZHALTER_DATENBANKNAME", null, 1);
+        super(context, DB_NAME, null, DB_VERSION);
         Log.d(LOG_TAG, "DbHelper hat die Datenbank: " + getDatabaseName() + " erzeugt.");
     }
 
