18c00571962307a61d7f5e3c87d949c0a814db4a
Frieza93 <f.a.jadau@gmx.de>
2018-05-01 15:29:43 +0200

    Back Commit

--- app/src/main/java/de/s/j/vorsorge_james/MainActivity.java
+++ app/src/main/java/de/s/j/vorsorge_james/MainActivity.java
@@ -2,17 +2,23 @@ package de.s.j.vorsorge_james;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Date;
+
+import de.s.j.vorsorge_james.database.dbKind.DbKindAccessWorker;
+import de.s.j.vorsorge_james.database.dbKind.DbKindDatensatz;
 
 public class MainActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
     /*
         Jannis und Frieza, maskuline Schläger,
         Körper definierter als der von Kollegah.
         Wann kriege ich meinen Echo?
      */
-=======
->>>>>>> origin/Dev-Jannis
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private DbKindAccessWorker dataSource;
 
 
 
@@ -20,5 +26,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DbKindDatensatz testMemo = new DbKindDatensatz(120, "Jannis", new Date(04/07/1989));
+        Log.d(LOG_TAG, "Inhalt der Testmemo: " + testMemo.toString());
+
+        dataSource = new DbKindAccessWorker(this);
     }
 }
