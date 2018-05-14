c6c7b9e64f22226220d757b8fb7fd4c6256bac0f
Frieza93 <f.a.jadau@gmx.de>
2018-05-14 10:37:38 +0200

    Rudimentary shape of ChildSelectionActivity

--- app/src/main/java/de/s/j/vorsorge_james/ChildSelectionActivity.java
+++ app/src/main/java/de/s/j/vorsorge_james/ChildSelectionActivity.java
@@ -1,10 +1,26 @@
 package de.s.j.vorsorge_james;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.List;
+import de.s.j.vorsorge_james.database.dbAccess;
+import de.s.j.vorsorge_james.database.dbKind.DbKindDatensatz;
+
 /**
  * Created by Frieza on 03.05.2018.
  */
 
-public class ChildSelectionActivity {
+public class ChildSelectionActivity extends AppCompatActivity {
+
+    private dbAccess dataSource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        this.dataSource = new dbAccess(this);
+        List<DbKindDatensatz> kinderListe = dataSource.getKindListe();
+        
+    }
 
 
 }
