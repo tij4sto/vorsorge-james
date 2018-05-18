07f410ba8a98395693e0075486f38739e27ad2ae
Frieza93 <f.a.jadau@gmx.de>
2018-05-18 11:54:40 +0200

    CrashedListView

--- app/src/main/java/de/s/j/vorsorge_james/childSelectionActivity/ListViewChildren.java
+++ app/src/main/java/de/s/j/vorsorge_james/childSelectionActivity/ListViewChildren.java
@@ -3,7 +3,6 @@ package de.s.j.vorsorge_james.childSelectionActivity;
 import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
-<<<<<<< HEAD
 import android.support.v7.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
